f4e1d2653e9361d40100cc1e18ea171fa235228b
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2018-07-30 15:10:57 +0200

    FIX init after copy const because some init generate const

--- script/reload-multientities.php
+++ script/reload-multientities.php
@@ -7,7 +7,34 @@
 	
 	set_time_limit(0);
 	
-	//unset($conf->file->dol_document_root['main']);
+	
+	
+	// Recopie des conf de l'entité 1 sur les autres entités
+	$sql = 'INSERT IGNORE INTO llx_const (name, entity, value, type, visible, note) ';
+	$sql .= 'SELECT c.name, e.rowid, c.value, c.type, c.visible, c.note ';
+	$sql .= 'FROM llx_const c, llx_entity e ';
+	$sql .= 'WHERE c.entity = 1 ';
+	$sql .= 'AND e.rowid != 1 ';
+	$db->query($sql);
+
+	// Recopie des droits groupes de l'entité 1 sur les autres
+	$sql = 'INSERT INTO llx_usergroup_rights (entity, fk_usergroup, fk_id) ';
+	$sql .= 'SELECT e.rowid, ur.fk_usergroup, ur.fk_id ';
+	$sql .= 'FROM llx_usergroup_rights ur, llx_entity e ';
+	$sql .= 'WHERE ur.entity = 1 ';
+	$sql .= 'AND e.rowid != 1 ';
+	$db->query($sql);
+
+	// Recopie des droits user de l'entité 1 sur les autres
+	$sql = 'INSERT INTO llx_user_rights (entity, fk_user, fk_id) ';
+	$sql .= 'SELECT e.rowid, ur.fk_user, ur.fk_id ';
+	$sql .= 'FROM llx_user_rights ur, llx_entity e ';
+	$sql .= 'WHERE ur.entity = 1 ';
+	$sql .= 'AND e.rowid != 1 ';
+	$db->query($sql);
+
+
+//unset($conf->file->dol_document_root['main']);
 	$modulesdir = dolGetModulesDirs();
 	
     $sql = "SELECT rowid, label FROM llx_entity ORDER BY rowid ASC";
@@ -81,26 +108,3 @@
     $ret = $object->switchEntity(1);
     $conf->setValues($db);
 
-// Recopie des conf de l'entité 1 sur les autres entités
-$sql = 'INSERT IGNORE INTO llx_const (name, entity, value, type, visible, note) ';
-$sql.= 'SELECT c.name, e.rowid, c.value, c.type, c.visible, c.note ';
-$sql.= 'FROM llx_const c, llx_entity e ';
-$sql.= 'WHERE c.entity = 1 ';
-$sql.= 'AND e.rowid != 1 ';
-$db->query($sql);
-
-// Recopie des droits groupes de l'entité 1 sur les autres
-$sql = 'INSERT INTO llx_usergroup_rights (entity, fk_usergroup, fk_id) ';
-$sql.= 'SELECT e.rowid, ur.fk_usergroup, ur.fk_id ';
-$sql.= 'FROM llx_usergroup_rights ur, llx_entity e ';
-$sql.= 'WHERE ur.entity = 1 ';
-$sql.= 'AND e.rowid != 1 ';
-$db->query($sql);
-
-// Recopie des droits user de l'entité 1 sur les autres
-$sql = 'INSERT INTO llx_user_rights (entity, fk_user, fk_id) ';
-$sql.= 'SELECT e.rowid, ur.fk_user, ur.fk_id ';
-$sql.= 'FROM llx_user_rights ur, llx_entity e ';
-$sql.= 'WHERE ur.entity = 1 ';
-$sql.= 'AND e.rowid != 1 ';
-$db->query($sql);
