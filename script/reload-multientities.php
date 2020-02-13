6c5a92f274ed6a53ce1c779d531fc8bf08e26e56
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-02-13 11:17:26 +0100

    FIX : Script reload-multientities, remove useless lines

--- script/reload-multientities.php
+++ script/reload-multientities.php
@@ -7,7 +7,6 @@ dol_include_once("/multicompany/class/actions_multicompany.class.php");
 
 set_time_limit(0);
 
-
 // Recopie des conf de l'entité 1 sur les autres entités
 $sql = 'INSERT IGNORE INTO llx_const (name, entity, value, type, visible, note) ';
 $sql .= 'SELECT c.name, e.rowid, c.value, c.type, c.visible, c.note ';
@@ -32,8 +31,6 @@ $sql .= 'WHERE ur.entity = 1 ';
 $sql .= 'AND e.rowid != 1 ';
 $db->query($sql);
 
-
-//unset($conf->file->dol_document_root['main']);
 $modulesdir = dolGetModulesDirs();
 
 $sql = "SELECT rowid, label FROM llx_entity ORDER BY rowid ASC";
@@ -48,13 +45,6 @@ if ($res) {
 $object = new ActionsMulticompany($db);
 $ret = $object->switchEntity(1);
 $conf->setValues($db);
-//    foreach ($TEntities as $e => $label){
-//        $object = new ActionsMulticompany($db);
-//        $ret = $object->switchEntity($e);
-
-//	$conf->entity = $e;
-
-//        echo '<h2>'.$label.'</h2>';
 
 if (! empty($conf->history->enabled)) {
     unset($conf->history->enabled, $conf->global->MAIN_MODULE_HISTORY);
@@ -63,19 +53,16 @@ if (! empty($conf->history->enabled)) {
 
 foreach ($modulesdir as $dir) {
     // Load modules attributes in arrays (name, numero, orders) from dir directory
-    //print $dir."\n<br>";
     dol_syslog("Scan directory ".$dir." for module descriptor files (modXXX.class.php)");
     $handle = @opendir($dir);
     if (is_resource($handle)) {
         while (($file = readdir($handle)) !== false) {
-            //print "$i ".$file."\n<br>";
             if (is_readable($dir.$file) && substr($file, 0, 3) == 'mod' && substr($file, dol_strlen($file) - 10) == '.class.php') {
                 $modName = substr($file, 0, dol_strlen($file) - 10);
 
                 $name = strtolower(preg_replace('/^mod/i', '', $modName));
 
                 if (! empty($conf->{$name}->enabled)) {
-
                     echo $name.'<br>';
 
                     //$res = unActivateModule($modName);
@@ -84,22 +71,14 @@ foreach ($modulesdir as $dir) {
                         $conf->entity = $e;
                         unset($conf->{$name}->enabled, $conf->global->{'MAIN_MODULE_'.strtoupper($name)});
                         $resarray = activateModule($modName);
-                        //var_dump($res, $resarray);exit;
 
                     }
                 }
-
             }
-
         }
-
     }
-
 }
-//  }
-
 
 $object = new ActionsMulticompany($db);
 $ret = $object->switchEntity(1);
 $conf->setValues($db);
-
