e511ae489bc7948e4b5f721f3d89b4bc9393f3c6
root <root@vps-90a32972.vps.ovh.net>
2023-08-31 17:11:21 +0200

    modifs pour desactivation modules CAVI

--- script/reload-multientities.php
+++ script/reload-multientities.php
@@ -10,7 +10,7 @@ set_time_limit(0);
 
 // Security check
 if (empty($user->admin)){
-	die('Administrator only');
+    die('Administrator only');
 }
 
 // Add un activate option
@@ -19,32 +19,32 @@ $unActivate = GETPOST('unactivate','int');
 // Manage dependency
 $dependency = 1;
 if(GETPOSTISSET('dependency')){
-	$dependency = GETPOST('dependency','int');
+    $dependency = GETPOST('dependency','int');
 }
 
-// Recopie des conf de l'entité 1 sur les autres entités
-$sql = 'INSERT IGNORE INTO llx_const (name, entity, value, type, visible, note) ';
-$sql .= 'SELECT c.name, e.rowid, c.value, c.type, c.visible, c.note ';
-$sql .= 'FROM llx_const c, llx_entity e ';
-$sql .= 'WHERE c.entity = 1 ';
-$sql .= 'AND e.rowid != 1 ';
-$db->query($sql);
+# // Recopie des conf de l'entité 1 sur les autres entités
+# $sql = 'INSERT IGNORE INTO llx_const (name, entity, value, type, visible, note) ';
+# $sql .= 'SELECT c.name, e.rowid, c.value, c.type, c.visible, c.note ';
+# $sql .= 'FROM llx_const c, llx_entity e ';
+# $sql .= 'WHERE c.entity = 1 ';
+# $sql .= 'AND e.rowid != 1 ';
+# $db->query($sql);
+# 
+# // Recopie des droits groupes de l'entité 1 sur les autres
+# $sql = 'INSERT INTO llx_usergroup_rights (entity, fk_usergroup, fk_id) ';
+# $sql .= 'SELECT e.rowid, ur.fk_usergroup, ur.fk_id ';
+# $sql .= 'FROM llx_usergroup_rights ur, llx_entity e ';
+# $sql .= 'WHERE ur.entity = 1 ';
+# $sql .= 'AND e.rowid != 1 ';
+# $db->query($sql);
 
-// Recopie des droits groupes de l'entité 1 sur les autres
-$sql = 'INSERT INTO llx_usergroup_rights (entity, fk_usergroup, fk_id) ';
-$sql .= 'SELECT e.rowid, ur.fk_usergroup, ur.fk_id ';
-$sql .= 'FROM llx_usergroup_rights ur, llx_entity e ';
-$sql .= 'WHERE ur.entity = 1 ';
-$sql .= 'AND e.rowid != 1 ';
-$db->query($sql);
-
-// Recopie des droits user de l'entité 1 sur les autres
-$sql = 'INSERT INTO llx_user_rights (entity, fk_user, fk_id) ';
-$sql .= 'SELECT e.rowid, ur.fk_user, ur.fk_id ';
-$sql .= 'FROM llx_user_rights ur, llx_entity e ';
-$sql .= 'WHERE ur.entity = 1 ';
-$sql .= 'AND e.rowid != 1 ';
-$db->query($sql);
+# // Recopie des droits user de l'entité 1 sur les autres
+# $sql = 'INSERT INTO llx_user_rights (entity, fk_user, fk_id) ';
+# $sql .= 'SELECT e.rowid, ur.fk_user, ur.fk_id ';
+# $sql .= 'FROM llx_user_rights ur, llx_entity e ';
+# $sql .= 'WHERE ur.entity = 1 ';
+# $sql .= 'AND e.rowid != 1 ';
+# $db->query($sql);
 
 $modulesdir = dolGetModulesDirs();
 
@@ -69,6 +69,17 @@ if (! empty($conf->history->enabled)) {
 }
 
 foreach ($modulesdir as $dir) {
+    echo $dir.'<br>';
+    if(
+        strpos($dir, 'custom/showprice') === false
+        && strpos($dir, 'custom/autoviewer') === false
+        && strpos($dir, 'custom/mylist') === false
+        && strpos($dir, 'custom/productreturns') === false
+        && strpos($dir, 'custom/cavi') === false
+        && strpos($dir, 'custom/pagessubstitution') === false
+    
+    ) continue;
+    echo '!<br>';
     // Load modules attributes in arrays (name, numero, orders) from dir directory
     dol_syslog("Scan directory ".$dir." for module descriptor files (modXXX.class.php)");
 
@@ -81,22 +92,27 @@ foreach ($modulesdir as $dir) {
                 $name = strtolower(preg_replace('/^mod/i', '', $modName));
                 if($name == 'propale') $name = 'propal';
 
-                if (! empty($conf->{$name}->enabled)) {
-                    echo $name.'<br>';
+                #if (! empty($conf->{$name}->enabled)) {
+                #    echo $name.'<br>';
 
                     foreach ($TEntities as $e => $label) {
                         $ret = $object->switchEntity($e);
                         $conf->setValues($db);
+                        if (empty($conf->{$name}->enabled)) {
+                            echo $conf->entity.':not enabled<br>';
+                            continue;
+                        }
 
-						if(!empty($unActivate)){
-							$res = unActivateModule($modName, $dependency);
-						}
+                        if(!empty($unActivate)){
+                            echo 'disabling '.$modName.' on entity '.$conf->entity.'<br>';
+                            $res = unActivateModule($modName, $dependency);
+                        }
 
                         unset($conf->{$name}->enabled, $conf->global->{'MAIN_MODULE_'.strtoupper($name)});
                         if($name == 'propal') unset($conf->global->MAIN_MODULE_PROPALE);
-                        $resarray = activateModule($modName);
+                        #$resarray = activateModule($modName);
                     }
-                }
+                #}
             }
         }
     }
